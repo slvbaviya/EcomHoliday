40474b9392a45665e798deeca7788180a037167b
vasanthaprabha.com <slvbaviya@gmail.com>
2024-09-24 22:47:37 +0530

    EComHoliday Project

---
+++ src/main/java/com/ecom/holiday/HolidayApplication.java
@@ -0,0 +1,14 @@
+package com.ecom.holiday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayApplication.class, args);
+		System.out.println("Hiii");
+	}
+
+}
